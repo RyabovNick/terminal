f655155c89861f6f363edfd1b4066e9bd358d29e
Ryabov Nick <nikitos.aka.zed@gmail.com>
2018-11-25 13:37:00 +0300

    uploadPics page added

--- server/index.js
+++ server/index.js
@@ -64,10 +64,11 @@ app.get('/api/getPictures', function(req, res, next) {
 app.get('/upload_form', function(req, res, next) {
     res.writeHead(200, { 'content-type': 'text/html' })
     res.end(
-        '<form action="/api/upload_videos" enctype="multipart/form-data" method="post">' +
+        '<form action="/api/upload_pictures" enctype="multipart/form-data" method="post">' +
             'Name: <input type="text" name="name"><br>' +
-            'DateFrom: <input type="datetime" name="dateFrom" value="2018-11-23 17:01:24"><br>' +
-            'DateTo: <input type="datetime" name="dateTo" value="2018-11-25 17:01:24"><br>' +
+            'DateFrom: <input type="datetime" name="date_start" value="2018-11-23 17:01:24"><br>' +
+            'DateTo: <input type="datetime" name="date_finish" value="2018-11-25 17:01:24"><br>' +
+            'DateTo: <input type="number" name="amount_time" value="2018-11-25 17:01:24"><br>' +
             '<input type="file" name="upload"><br>' +
             '<input type="submit" value="Upload">' +
             '</form>'
@@ -121,6 +122,69 @@ app.post('/api/upload_videos', function(req, res, next) {
                     con.query(
                         'Insert into `videos` (name,link) values (?,?)',
                         [el.file.name, el.file.path.substr(8)],
+                        function(error, result) {
+                            if (error) return res.status(406).send(error)
+                            console.log(result)
+                            query_result.push(result)
+                        }
+                    )
+                })
+                con.release()
+            })
+            res.writeHead(200, { 'content-type': 'text/plain' })
+            res.end()
+        })
+})
+
+app.post('/api/upload_pictures', function(req, res, next) {
+    var form = new formidable.IncomingForm(),
+        files = {},
+        fields = {},
+        allFiles = []
+
+    form.uploadDir = '../html/files'
+    //form.uploadDir = './files'
+    form.keepExtensions = true
+    form.multiples = true
+
+    var query_result = [] //save all insert responses
+
+    form.parse(req)
+        .on('file', function(name, file) {
+            files[name] = file
+            allFiles.push({ name, file })
+        })
+        .on('field', function(name, field) {
+            fields[name] = field
+        })
+        .on('error', function(err) {
+            next(err)
+        })
+        .on('end', function() {
+            if (fields.name == '') {
+                fields.name = files.upload.name
+            }
+            pool.getConnection(function(err, con) {
+                if (err) return res.status(406).send(err)
+                /*array1.forEach(function(element) {
+				    console.log(element);
+				});
+				*/
+                allFiles.forEach(function(el) {
+                    //console.log('fields.dateFrom: ', fields.dateFrom)
+                    //console.log('fields.dateTo: ', fields.dateTo)
+                    console.log('el.file.name: ', el.file.name)
+                    console.log('el.file.path: ', el.file.path)
+                    //в ссылке лишнее (../html/) поэтому substr(8)
+                    con.query(
+                        'Insert into `pictures` (name,link,date_start,date_finish,amount_time) values (?,?,?,?,?)',
+                        [
+                            el.file.name,
+                            el.file.path.substr(8),
+                            fields.date_start,
+                            fields.date_finish,
+                            fields.amount_time
+                        ],
                         function(error, result) {
                             if (error) return res.status(406).send(error)
                             console.log(result)
